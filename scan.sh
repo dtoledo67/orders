086382f852a8deedeb2164fbfb4bd978d5365c8b
Damien Toledo <damien@nirmata.com>
2024-03-13 10:20:47 +0530

    init

---
+++ scan.sh
@@ -0,0 +1,4 @@
+export NIRMATA_TOKEN=ZseonWElsU3+MOb1W8TC81PgEuHT66/2fhBlwBsKDis0oauBuG/XXNH8r1WX7On2JqtwOOVz3ezDhwzM0EgzIg==
+export NIRMATA_URL=https://devtest2.nirmata.co
+
+nctl scan git-repo --policies ./policy --publish --token ZseonWElsU3+MOb1W8TC81PgEuHT66/2fhBlwBsKDis0oauBuG/XXNH8r1WX7On2JqtwOOVz3ezDhwzM0EgzIg==
